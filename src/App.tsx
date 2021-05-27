84f5d3783fce714241b65ae5c847ce699f93191a
Langer <bruno.langer@erzinger.com.br>
2021-05-27 19:38:05 -0300

    Footer and Members

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Components/Frames/Header";
+import Footer from "./Components/Frames/Footer";
 import Robotica from "./Pages/Robotica";
 import Arduino from "./Pages/Arduino";
 import styled from "styled-components";
@@ -38,9 +39,36 @@ function App() {
         <Robotica />
         <Robotica id="projeto" />
         <Robotica id="projeto" />
+        <Footer/>
       </MainContent>
     </>
   );
 }
 
 export default App;
+
+
+
+// [19:18] DANIEL MANIGLIA AMANCIO DA SILVA
+// N2 - Descrição das atividades e avaliação: 
+
+// [19:20] DANIEL MANIGLIA AMANCIO DA SILVA
+// 1. Logo do grupo e nome do grupo. (1,0)
+
+// [19:20] DANIEL MANIGLIA AMANCIO DA SILVA
+// 2. Integrantes dos grupo. (1,0)
+
+// [19:20] DANIEL MANIGLIA AMANCIO DA SILVA
+// 3. Uma introdução sobre o grupo. (2,0)
+
+// [19:20] DANIEL MANIGLIA AMANCIO DA SILVA
+// 4. Vídeo institucional por que robótica na TI?
+// (Tem que ter com uma breve explicação do trabalho) (2,0)
+
+// [19:21] DANIEL MANIGLIA AMANCIO DA SILVA
+// 5. Projeto desenvolvido do Tinkercad sobre o seguidor de linha.
+// (Tem que ter uma breve explicação do projeto) (2,0)
+
+// [19:21] DANIEL MANIGLIA AMANCIO DA SILVA
+// 6. Avaliação do desenvolvimento do Website (2,0)
+
---
+++ src/Components/Frames/Footer.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const WrapFooter = styled.footer`
+background-color: #232323;
+height: 60vh;
+`;
+
+export default function () {
+  return <WrapFooter></WrapFooter>;
+}
