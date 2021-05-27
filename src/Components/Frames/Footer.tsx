81a64580a9433ea19193b720f651ef4db4be2721
Langer <bruno.langer@erzinger.com.br>
2021-05-27 20:43:17 -0300

    Member fix and Footer

--- src/Components/Frames/Footer.tsx
+++ src/Components/Frames/Footer.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
-import {ReactComponent as Logo} from "../../img/nav-logo.svg";
-import Thuany from '../../img/thuany.jpeg'
+import { ReactComponent as Logo } from "../../img/nav-logo.svg";
+import Thuany from "../../img/thuany.png";
+import Langer from "../../img/langer.jpg";
+import Kleber from "../../img/kleber.png";
+import Josue from "../../img/josue.png";
+import Julio from "../../img/julio.png";
+import Member from "./Member";
 
 const WrapFooter = styled.footer`
   background-color: #232323;
@@ -10,12 +15,45 @@ const WrapFooter = styled.footer`
   justify-content: space-evenly;
 `;
 
-
+const GridMembers = styled.div`
+  width: 70%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 
 export default function () {
   return (
     <WrapFooter>
-      <Logo fill="white" stroke="white"/>
+      <GridMembers>
+        <Member
+          name="Thuany Souza"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          img={Thuany}
+        />
+        <Member
+          name="Josué Laurindo"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          img={Josue}
+        />
+        <Member
+          name="Kleber Keuze"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          img={Kleber}
+        />
+        <Member
+          name="Bruno Langer"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          img={Langer}
+        />
+        <Member
+          name="Julio Marcon"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+          img={Julio}
+        />
+      </GridMembers>
+      <Logo fill="white" />
     </WrapFooter>
   );
 }
--- src/Components/Frames/Member.tsx
+++ src/Components/Frames/Member.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import styled from "styled-components";
 
 interface memberProps {
   name: string;
@@ -6,14 +7,46 @@ interface memberProps {
   img: string;
 }
 
+
+
+const WrapMember = styled.div`
+  background-color: #292929;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  max-width: 30vw;
+  height: 12vh;
+  padding: 0.3rem;
+  color: white;
+  border-radius: 0.5rem;
+  box-shadow: 20px 20px 60px #101010, -20px -20px 60px #6a6a6a33;
+  font-size: 0.8rem;
+  margin: 1rem;
+  /* border:solid 1px white; */
+`;
+
+const PersonIcon = styled.img`
+  height: 90%;
+  border-radius: 50%;
+  /* object-fit: contain; */
+`;
+
+const WrapText = styled.aside`
+  max-width: 70%;
+  max-height: 90%;
+
+  strong{
+      font-size:1.1rem
+  }
+`;
 export default function Member({ name, description, img }: memberProps) {
   return (
-    <div>
-      <img src={img} alt="" />
-      <div>
+    <WrapMember>
+      <PersonIcon src={img} alt="" />
+      <WrapText>
         <strong>{name}</strong>
-        <span>{description}</span>
-      </div>
-    </div>
+        <p>{description}</p>
+      </WrapText>
+    </WrapMember>
   );
 }
